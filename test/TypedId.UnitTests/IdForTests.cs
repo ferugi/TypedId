a31e15d31ad49f23760b850aca64aeb3f3a93361
Ferugi El Heri <me@ferugi.com>
2020-06-14 19:58:41 +0100

    Moved source into src/ folder and added unit test project.

---
+++ test/TypedId.UnitTests/IdForTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using TypedId;
+using TypedId.Examples;
+using Xunit;
+
+namespace TypedIdTests
+{
+    public class IdForTests
+    {
+
+        public Task IdForT_WithEmptyString_ReturnsNewId()
+        {
+            // Arrange
+            var idInput = string.Empty;
+
+            // Act
+            var id = IdFor<Person>.Wrap(idInput);
+
+            Assert.NotNull
+
+        }
+    }
+}
